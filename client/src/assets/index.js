290f7e2e4fd202ab7584fff1ab807a191a90e53a
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-11 15:10:10 +0530

    deploy2

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,7 @@
-import { Languages } from './Constants/LanguageOptions.js';
-import { DefaultCode } from './Constants/DefaultCode.js';
-import DeleteIcon from './Images/delete.png';
-import Logo from './Images/web-coding.png';
+import { Languages } from './constants/LanguageOptions.js';
+import { DefaultCode } from './constants/DefaultCode.js';
+import DeleteIcon from './images/delete.png';
+import Logo from './images/web-coding.png';
 
 export {
     Languages,
