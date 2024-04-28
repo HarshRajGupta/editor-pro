0792bbccea367d29fce256d3d5ddc2cb20c7aeae
HarshRajGupta <HarshRajGupta@hotmail.com>
2024-04-28 20:30:53 +0530

    add transactions

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,49 +3,53 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const bcryptSalt = bcrypt.genSaltSync(10);
 const { registrationMail } = require("./mail");
+const { pg } = require("../config");
 
 const register = async (req, res) => {
     try {
-        const { name, password } = req.body;
-        const email = req.body.email.toLowerCase();
-        if (!name || !email || !password) return res.status(400).json({ success: false, message: "Bad Request" });
-        const prevUser = await User.findOne({ where: { email } });
-        if (prevUser) return res.status(400).json({ success: false, message: "User already registered" });
-        let user = req.user;
-        if (user) {
-            await user.update({ name, email, password: bcrypt.hashSync(password, bcryptSalt) });
-        } else {
-            user = await User.create({
-                name,
-                email,
-                password: bcrypt.hashSync(password, bcryptSalt),
-            });
-        }
-        await Connection.update(
-            { userId: user.id },
-            { where: { email } }
-        )
-        registrationMail(email, name);
-        jwt.sign(
-            { id: user.id },
-            process.env.JWT_SECRET,
-            (err, token) => {
-                if (err) throw err;
-                return res.status(200).json({
-                    success: true,
-                    message: "Registration Successful",
-                    user: user,
-                    token: token,
-                });
-            },
-        );
+        await pg.transaction(async (t) => {
+            const { name, password } = req.body;
+            const email = req.body.email.toLowerCase();
+            if (!name || !email || !password) return res.status(400).json(
+                { success: false, message: "Bad Request" }
+            );
+            const prevUser = await User.findOne(
+                { where: { email } }
+            );
+            if (prevUser) return res.status(400).json(
+                { success: false, message: "User already registered" }
+            );
+            let user = req.user;
+            if (user) await user.update(
+                { name, email, password: bcrypt.hashSync(password, bcryptSalt) },
+                { transaction: t }
+            );
+            else user = await User.create(
+                { name, email, password: bcrypt.hashSync(password, bcryptSalt) },
+                { transaction: t }
+            );
+            await Connection.update(
+                { userId: user.id },
+                { where: { email } },
+                { transaction: t }
+            )
+            registrationMail(email, name);
+            jwt.sign(
+                { id: user.id },
+                process.env.JWT_SECRET,
+                (err, token) => {
+                    if (err) throw err;
+                    return res.status(200).json(
+                        { success: true, message: "Registration Successful", user: user, token: token }
+                    );
+                },
+            );
+        })
     } catch (err) {
         console.error("DEBUG: Error while registration", err);
-        return res.status(422).json({
-            success: false,
-            message: "Registration Failed",
-            error: err,
-        });
+        return res.status(422).json(
+            { success: false, message: "Registration Failed", error: err }
+        );
     }
 };
 
@@ -62,66 +66,61 @@ const login = async (req, res) => {
                     process.env.JWT_SECRET,
                     (err, token) => {
                         if (err) throw err;
-                        return res.status(200).json({
-                            success: true,
-                            message: "Logged in Successfully",
-                            user: user,
-                            token: token,
-                        });
+                        return res.status(200).json(
+                            { success: true, message: "Logged in Successfully", user: user, token: token }
+                        );
                     },
                 );
-            } else {
-                return res.status(422).json({ success: false, message: "Incorrect Password" });
-            }
-        } else {
-            return res.status(404).json({ success: false, message: "User not found" });
-        }
+            } else return res.status(422).json(
+                { success: false, message: "Incorrect Password" }
+            );
+        } else return res.status(404).json(
+            { success: false, message: "User not found" }
+        );
     } catch (err) {
         console.error("DEBUG: Error logging in", err);
-        return res
-            .status(404)
-            .json({ success: false, message: "Bad request", error: err });
+        return res.status(404).json(
+            { success: false, message: "Bad request", error: err }
+        );
     }
 };
 
 const verify = async (req, res) => {
     try {
         const user = req.user
-        res.status(200).json({ success: true, message: `Welcome ${user.name}`, user });
+        res.status(200).json(
+            { success: true, message: `Welcome ${user.name}`, user }
+        );
     } catch (err) {
         console.error("DEBUG: Error while verifying token", err);
-        return res.status(500).json({ success: false, error: err });
+        return res.status(500).json(
+            { success: false, error: err }
+        );
     }
 };
 
 const guest = async (req, res) => {
     try {
-        var user = req.user;
-        if (!user) {
-            user = await User.create({})
-        }
+        let user = req.user;
+        if (!user) user = await User.create({})
         jwt.sign(
             { id: user.id },
             process.env.JWT_SECRET,
             (err, token) => {
                 if (err) throw err;
-                res.cookie("guestId", token, {
-                    secure: true,
-                    httpOnly: true
-                });
-                return res.status(200).json({
-                    success: true,
-                    message: "Guest Logged in Successfully",
-                    user: user,
-                    token: token,
-                });
+                res.cookie("guestId", token,
+                    { secure: true, httpOnly: true }
+                );
+                return res.status(200).json(
+                    { success: true, message: "Guest Logged in Successfully", user: user, token: token }
+                );
             },
         );
     } catch (err) {
         console.error("Error: Guest logging in", err);
-        return res
-            .status(400)
-            .json({ success: false, message: "Bad request", error: err });
+        return res.status(400).json(
+            { success: false, message: "Bad request", error: err }
+        );
     }
 };
 
--- server/controllers/document.js
+++ server/controllers/document.js
@@ -1,5 +1,6 @@
 const { User, Document, Connection } = require("../models");
 const { invitationMail } = require("./mail");
+const { pg } = require("../config");
 require("dotenv").config();
 const sources = JSON.parse(process.env.SOURCES || "[]");
 
@@ -9,32 +10,29 @@ const getById = async (req, res) => {
 
 const create = async (req, res) => {
     try {
-        const { type, name } = req.body;
-        const user = req.user;
-        if (!type || !name)
-            return res.status(400).json({ success: false, message: "Please provide a valid type and name" });
-        const newDocument = await Document.create({
-            type,
-            ownerId: user.id,
-            name,
-            source: sources[Math.floor(Math.random() * sources.length)],
-        });
-        await Connection.create({
-            documentId: newDocument.id,
-            userId: user.id,
-            email: user.email
-        });
-        return res.status(200).json({
-            success: true,
-            message: "Document created",
-            document: newDocument,
-        });
+        await pg.transaction(async (t) => {
+            const { type, name } = req.body;
+            const user = req.user;
+            if (!type || !name) return res.status(400).json(
+                { success: false, message: "Please provide a valid type and name" }
+            );
+            const newDocument = await Document.create(
+                { type, ownerId: user.id, name, source: sources[Math.floor(Math.random() * sources.length)] },
+                { transaction: t }
+            );
+            await Connection.create(
+                { documentId: newDocument.id, userId: user.id, email: user.email },
+                { transaction: t }
+            );
+            return res.status(200).json(
+                { success: true, message: "Document created", document: newDocument }
+            );
+        })
     } catch (e) {
         console.error("ERROR: while creating document", e);
-        return res.status(400).json({
-            success: false,
-            message: "Something went wrong!! Please try again",
-        });
+        return res.status(400).json(
+            { success: false, message: "Something went wrong!! Please try again" }
+        );
     }
 };
 
@@ -42,17 +40,14 @@ const get = async (req, res) => {
     try {
         const user = req.user;
         const documents = await user.getDocuments();
-        return res.status(200).json({
-            success: true,
-            message: "Documents fetched",
-            documents: documents,
-        });
+        return res.status(200).json(
+            { success: true, message: "Documents fetched", documents: documents }
+        );
     } catch (e) {
         console.error("ERROR: while searching documents", e);
-        return res.status(500).json({
-            success: false,
-            message: "Something went wrong!! Please try again",
-        });
+        return res.status(500).json(
+            { success: false, message: "Something went wrong!! Please try again" }
+        );
     }
 };
 
@@ -60,110 +55,102 @@ const invite = async (req, res) => {
     try {
         const user = req.user;
         const email = req.body.email.toLowerCase();
-        if (!email) return res.status(400).json({ success: false, message: "Please provide a valid email" });
+        if (!email) return res.status(400).json(
+            { success: false, message: "Please provide a valid email" }
+        );
         const document = req.document;
         const invitedUser = await User.findOne({ where: { email } })
-        if (invitedUser)
-            await Connection.create({ documentId: document.id, userId: invitedUser.id, email });
-        else
-            await Connection.create({ documentId: document.id, email: email });
-
-        invitationMail(
-            user.email,
-            email,
-            document.owner.email,
-            document.name,
-            document.id,
+        if (invitedUser) await Connection.create(
+            { documentId: document.id, userId: invitedUser.id, email }
+        )
+        else await Connection.create(
+            { documentId: document.id, email: email }
         );
-        return res.status(200).json({ success: true, message: `${newEmail} Invited` });
+
+        invitationMail(user.email, email, document.owner.email, document.name, document.id);
+        return res.status(200).json(
+            { success: true, message: `${newEmail} Invited` }
+        );
     } catch (e) {
         console.error("ERROR: while adding user", e);
-        return res.status(500).json({
-            success: false,
-            message: "Something went wrong!! Please try again",
-        });
+        return res.status(500).json(
+            { success: false, message: "Something went wrong!! Please try again" }
+        );
     }
 };
 
 const destroy = async (req, res) => {
     try {
         const document = req.ownedDocument;
-        if (!document) return res.status(404).json({ success: false, message: "Document not found" });
-        await document.destroy();
-        return res.status(200).json({ success: true, message: "Document deleted" });
+        if (!document) return res.status(404).json(
+            { success: false, message: "Document not found" }
+        );
+        await pg.transaction(async (t) => await document.destroy({ transaction: t }))
+        return res.status(200).json(
+            { success: true, message: "Document deleted" }
+        );
     } catch (e) {
         console.error("ERROR: while deleting document", e);
-        return res.status(400).json({
-            success: false,
-            message: "Something went wrong!! Please try again",
-        });
+        return res.status(400).json(
+            { success: false, message: "Something went wrong!! Please try again" }
+        );
     }
 };
 
 const updateType = async (req, res) => {
     try {
         const { type } = req.body;
-        if (!type) return res.status(400).json({ success: false, message: "Please provide a valid type" });
+        if (!type) return res.status(400).json(
+            { success: false, message: "Please provide a valid type" }
+        );
         const document = req.document;
-        await document.update({
-            type
-        });
-        return res.status(200).json({ success: true, message: "Document type changed" });
+        await document.update(
+            { type }
+        );
+        return res.status(200).json(
+            { success: true, message: "Document type changed" }
+        );
     } catch (e) {
         console.error("ERROR: while changing document type", e);
-        return res.status(500).json({
-            success: false,
-            message: "Something went wrong!! Please try again",
-        });
+        return res.status(500).json(
+            { success: false, message: "Something went wrong!! Please try again" }
+        );
     }
 };
 
 const updateStatus = async (req, res) => {
     try {
         const { status } = req.body;
-        if (status === null || status === undefined)
-            return res.status(400).json({
-                success: false,
-                message: "Please provide a valid status"
-            });
+        if (status === null || status === undefined) return res.status(400).json(
+            { success: false, message: "Please provide a valid status" }
+        );
         const document = req.document;
-        await document.update({
-            status
-        });
-        return res.status(200).json({ success: true, message: "Document openToAll changed" });
+        await document.update(
+            { status }
+        );
+        return res.status(200).json(
+            { success: true, message: "Document openToAll changed" }
+        );
     } catch (e) {
         console.error("ERROR: while changing document openToAll", e);
-        return res.status(500).json({
-            success: false,
-            message: "Something went wrong!! Please try again",
-        });
+        return res.status(500).json(
+            { success: false, message: "Something went wrong!! Please try again" }
+        );
     }
 };
 
 const getStatus = async (req, res) => {
     try {
         const document = req.document;
-        return res.status(200).json({
-            success: true,
-            message: "Document status fetched",
-            status: document.status,
-        });
+        return res.status(200).json(
+            { success: true, message: "Document status fetched", status: document.status }
+        );
     } catch (e) {
         console.error("ERROR: while fetching document status", e);
-        return res.status(500).json({
-            success: false,
-            message: "Something went wrong!! Please try again",
-        });
+        return res.status(500).json(
+            { success: false, message: "Something went wrong!! Please try again" }
+        );
     }
-
 }
 
-module.exports = {
-    create,
-    get,
-    invite,
-    getById,
-    destroy,
-    updateType,
-    updateStatus,
-};
+module.exports = { create, get, invite, getById, destroy, updateType, updateStatus, getStatus };
