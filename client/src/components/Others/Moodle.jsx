d8dcc8208227342b323b594b141aef13be62761d
HarshRajGupta <HarshRajGupta@hotmail.com>
2024-05-01 21:10:21 +0530

    bugfix-7

--- client/src/components/Others/Moodle.jsx
+++ client/src/components/Others/Moodle.jsx
@@ -121,7 +121,7 @@ function Moodle({ setShowMoodle }) {
 									window.location.href,
 								);
 							}}
-							className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-1"
 						>
 							Copy Link
 						</button>
