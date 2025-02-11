69d3934cbc7d1bfee00f0f1ffd92a7fde5fb8693
HarshRajGupta <HarshRajGupta@hotmail.com>
2025-02-12 00:47:28 +0530

    new build - 2

--- client/src/components/Document/index.jsx
+++ client/src/components/Document/index.jsx
@@ -14,13 +14,11 @@ const stats = (interval) => {
   }
 }
 
-function debounce(func, delay) {
+const debounce = (func, delay) => {
 	let timeout = null;
 	return () => {
 		if (timeout) clearTimeout(timeout);
-		timeout = setTimeout(() => {
-			func();
-		}, delay);
+		timeout = setTimeout(() => func, delay);
 	};
 }
 
